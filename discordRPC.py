7666790d4280e0fb748fdeeb2281c99cad0932d8
popcorn <popcorn9499@gmail.com>
2022-11-10 22:15:14 -0600

    added text features

--- discordRPC.py
+++ discordRPC.py
@@ -5,6 +5,7 @@ class discordRPC:
         self.client_id = client_id
         self.rpc = Presence(self.client_id)
         self.presenceCleared = True
+        print("LAUNCHING PRESENCE")
         self.connect()
 
     def connect(self):
@@ -13,13 +14,14 @@ class discordRPC:
         except:
             ConnectionErrorDiscordRPC()
 
-    def setPresence(self,state=None,details=None,large_image=None,small_image=None,startTime=None,endTime=None):
+    def setPresence(self,state=None,details=None,large_text=None,large_image=None,small_text=None,small_image=None,startTime=None,endTime=None):
+        print("Setting presence")
         try:
-            self.rpc.update(state=state,details=details,large_image=large_image,small_image=small_image,start=startTime,end=endTime)
+            self.rpc.update(state=state,details=details,large_text=large_text,large_image=large_image,small_text=small_text,small_image=small_image,start=startTime,end=endTime)
             self.presenceCleared = False
         except:
             self.connect()
-            self.setPresence(self,state=state,details=details, large_image=large_image, small_image=small_image, startTime=startTime, endTime=endTime)
+            self.setPresence(self,state=state,details=details,large_text=large_text, large_image=large_image,small_text=small_text, small_image=small_image, startTime=startTime, endTime=endTime)
             
     def clear(self):
         if not self.presenceCleared:
