ed1e8b5e9062b029abdcfe04ac9105976261b1a1
popcorn <popcorn9499@gmail.com>
2022-11-10 21:34:47 -0600

    copy of a old discordRPC codebase
    this was stolen from another one of my projects

---
+++ discordRPC.py
@@ -0,0 +1,41 @@
+from pypresence import Presence
+
+class discordRPC:
+    def __init__(self,client_id):
+        self.client_id = client_id
+        self.rpc = Presence(self.client_id)
+        self.presenceCleared = True
+        self.connect()
+
+    def connect(self):
+        try:
+            self.rpc.connect() 
+        except:
+            ConnectionErrorDiscordRPC()
+
+    def setPresence(self,state=None,details=None,large_image=None,small_image=None,startTime=None,endTime=None):
+        try:
+            self.rpc.update(state=state,details=details,large_image=large_image,small_image=small_image,start=startTime,end=endTime)
+            self.presenceCleared = False
+        except:
+            self.connect()
+            
+    def clear(self):
+        if not self.presenceCleared:
+            try:
+                self.rpc.clear()
+                self.presenceCleared = True
+            except:
+                self.connect()
+                
+class NoStatus():
+    pass
+
+class NoSong(Exception):
+    pass
+
+class ConnectionErrorMPD(Exception):
+    pass
+
+class ConnectionErrorDiscordRPC(Exception):
+    pass
